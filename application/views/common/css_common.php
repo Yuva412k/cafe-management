fa30381f5ac567077f553f9ff976284626ad77fe
Yuva412k <72986885+Yuva412k@users.noreply.github.com>
2021-06-19 15:36:06 +0530

    Update css_common.php

--- application/views/common/css_common.php
+++ application/views/common/css_common.php
@@ -1,8 +1,8 @@
 
-<link rel="stylesheet" href="<?php echo PUBLIC_ROOT?>Plugins/select2/select2.min.css">
-<link rel="stylesheet" href="<?php echo PUBLIC_ROOT?>Plugins/toastr/toastr.css">
-<link rel="stylesheet" href="<?php echo PUBLIC_ROOT?>Plugins/fontawesome/css/all.css">
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
 <link rel="stylesheet" href="<?php echo PUBLIC_ROOT?>css/style.css">
 <link rel="stylesheet" href="<?php echo PUBLIC_ROOT?>css/table.css">
-<link rel="stylesheet" href="<?php echo PUBLIC_ROOT?>Plugins/DataTables/DataTables-1.10.24/css/dataTables.bootstrap4.min.css">
-<link rel="stylesheet" href="<?php echo PUBLIC_ROOT?>Plugins/jquery-ui/jquery-ui.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" integrity="sha512-aOG0c6nPNzGk+5zjwyJaoRUgCdOrfSDhmMID2u4+OIslr0GjpLKo7Xm0Ao3xmpM4T8AmIouRkqwj1nrdVsLKEQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />

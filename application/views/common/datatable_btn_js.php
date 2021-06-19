c6eea984f57c6d88e2a74e54171e7e4e9b7d006e
Yuva412k <72986885+Yuva412k@users.noreply.github.com>
2021-06-19 15:39:44 +0530

    Update datatable_btn_js.php

--- application/views/common/datatable_btn_js.php
+++ application/views/common/datatable_btn_js.php
@@ -1,7 +1,7 @@
 
-    <script src="<?php echo PUBLIC_ROOT.'Plugins/DataTables/Buttons-1.7.0/js/dataTables.buttons.min.js'?>"></script>
-    <script src="<?php echo PUBLIC_ROOT.'Plugins/DataTables/JSZip-2.5.0/jszip.min.js'?>"></script>
-    <script src="<?php echo PUBLIC_ROOT.'Plugins/DataTables/pdfmake-0.1.36/pdfmake.min.js'?>"></script>
-    <script src="<?php echo PUBLIC_ROOT.'Plugins/DataTables/pdfmake-0.1.36/vfs_fonts.js'?>"></script>
-    <script src="<?php echo PUBLIC_ROOT.'Plugins/DataTables/Buttons-1.7.0/js/buttons.html5.min.js'?>"></script>
-    <script src="<?php echo PUBLIC_ROOT.'Plugins/DataTables/Buttons-1.7.0/js/buttons.print.min.js'?>"></script>
+    <script src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js"></script>

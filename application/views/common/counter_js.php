78438a082ebc8bc1c3edbbfd4c175963cd5b0e3d
Yuva412k <72986885+Yuva412k@users.noreply.github.com>
2021-06-19 15:29:22 +0530

    Update counter_js.php

--- application/views/common/counter_js.php
+++ application/views/common/counter_js.php
@@ -1,6 +1,6 @@
-<script src="<?php echo PUBLIC_ROOT.'Plugins/jquery/jquery-2.1.3.min.js'?>"></script>
-<script src="<?php echo PUBLIC_ROOT.'Plugins/jquery/jquery.counterup.min.js'?>"></script>
-<script src="<?php echo PUBLIC_ROOT.'Plugins/jquery/waypoints2.0.3.min.js'?>"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.counterup/1.0/jquery.counterup.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
 
 <script >
     let jQuery_2_1_3 = $.noConflict(true)
